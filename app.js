5392d768b95677a6e85f69c59b8fa7af16da7f31
Mustfa1122 <145039363+Mustfa1122@users.noreply.github.com>
2023-11-04 05:55:42 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var Food=prompt("enter your food ")
+if(Food==="pizza"){
+    alert("1 lay ao")
+}else{
+    alert("only available pizza")
+}
+var Flavour=prompt("enter your flavour")
+if (Flavour=="bbq") {
+    alert("done")
+    
+} else {
+    alert("only bbq flavour")
+    
+}
+var size=prompt("enter your size")
+if(size==="small"){
+    alert("yes")
+}else{
+    alert("only small size available")
+}
